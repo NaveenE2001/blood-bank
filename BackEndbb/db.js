af55b6a8d5df8db7cb50c77880804c8af968bb3d
NaveenE2001 <112232918+NaveenE2001@users.noreply.github.com>
2025-12-12 15:59:04 +0530

    Update MongoDB connection URI

--- BackEndbb/db.js
+++ BackEndbb/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb+srv://naveen:naveen@newcluster.sitkzn8.mongodb.net/test";
-//bloodbank? try="mongodb+srv://bloodbank?naveen:naveen@newcluster.sitkzn8.mongodb.net/test";
+  "mongodb+srv://naveen:naveen@newcluster."
 const connectToMongo = () => {
   mongoose.set("strictQuery", true);
   mongoose.connect(mongoURI, () => {
